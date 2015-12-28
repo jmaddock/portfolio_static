090cab6cf465d5e5e63b0406c03edd642733c888
Jim Maddock <skimaddster@gmail.com>
2015-12-28 09:42:26 -0500

    first commit!

---
+++ main.py
@@ -0,0 +1,15 @@
+import os, urllib, jinja2, webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render({}))
+
+application = webapp2.WSGIApplication([
+    ('/*', MainPage),
+], debug=True)
